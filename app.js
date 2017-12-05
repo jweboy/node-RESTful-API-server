16368fbeb8bdbadbf71d6b422286431e3f21a7b9
jianglei <1259524459@qq.com>
2017-12-05 16:18:19 +0800

    debugger route

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 const Koa = require('koa')
 const router = require('./routes')
 
-function startServer() {
-  // Initial variable
-  const app = new Koa()
-  const isDev = process.env.NODE_ENV === 'production'
-  const port = process.env.PORT || 3000
-  const host = process.env.HOST || '127.0.0.1'
+// Initial variable
+const app = new Koa()
+// const isDev = process.env.NODE_ENV === 'production'
+// const port = process.env.PORT || 3000
+// const host = process.env.HOST || '127.0.0.1'
 
-  // Set koa router
-  router.get('/', (ctx) => {
-    ctx.body = 'Node Koa API开发测试'
-  })
-  app
-    .use(router.routes())
-    .use(router.allowedMethods())
+// Set koa router
+router.get('/', (ctx) => {
+  ctx.body = 'Node Koa API开发测试'
+})
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
-  app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-  });
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
 
 
-  // Listen the server
-  app.listen(port, host)
-  console.log(`Server listening on port ${host}:${port}!`)
-}
+// Listen the server
+// app.listen(port, host)
+// console.log(`Server listening on port ${host}:${port}!`)
 
-startServer()
+module.exports = app
