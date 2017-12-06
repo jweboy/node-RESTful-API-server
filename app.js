1517bc83e890de0b82cdcad7ffbaa461cf9e9d54
jianglei <1259524459@qq.com>
2017-12-06 15:54:03 +0800

    add: logger and 404

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Koa = require('koa')
+const logger = require('koa-logger')
 const router = require('./routes')
+const { NotFound } = require('./middleware/not-found')
 
 function startServer() {
   // Initial variable
-  const app = new Koa()
+  const app = module.exports = new Koa()
   const isDev = process.env.NODE_ENV === 'production'
   const port = process.env.PORT || 3000
   const host = process.env.HOST || '127.0.0.1'
@@ -13,15 +15,10 @@ function startServer() {
     ctx.body = 'Node Koa API开发测试'
   })
   app
+    .use(logger())
     .use(router.routes())
     .use(router.allowedMethods())
-
-  app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-  });
+    .use(NotFound)
 
 
   // Listen the server
---
+++ middleware/not-found.js
@@ -0,0 +1,22 @@
+async function NotFound(ctx) {
+  // We need to explicitly set 404 here
+  // So that koa doesn't assign 200 on body
+  ctx.status = 404
+
+  switch (ctx.accepts('html', 'json')) {
+    case 'html':
+      ctx.type = 'html'
+      ctx.body = '<p>Page Not Found => HTML</p>'
+      break
+    case 'json':
+      ctx.body = {
+        message: 'Page Not Found => JSON'
+      }
+      break
+    default:
+      ctx.type = 'text'
+      ctx.body = 'Page Not Found => TEXT'
+  }
+}
+
+exports.NotFound = NotFound
