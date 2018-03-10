4c04aef2ce6f53a399fc8133b0380141f1c589fd
jianglei <jweboy0630@gmail.com>
2018-03-10 16:47:33 +0800

    update mongodb config

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const fastify = require('fastify')({
-  logger: true
+  // logger: true
 })
+// const mongodb = require('mongodb')
+// mongodb.MongoClient.connect('mongodb://jweboy:jl940630@ds149743.mlab.com:49743/myapp')
+//   .then(client => {
+//     const fastify = require('fastify')()
+//     fastify.register(goodsRoute)
+//     fastify.register(
+//       require('fastify-mongodb'),
+//       { client }
+//     )
+//   })
+const util = require('util')
 const mongodb = require('fastify-mongodb')
+const mongoConfig = require('./config/mongodb.json')
 const {
   goodsRoute
 } = require('./route')
 
 // mongodb register
+const mongoUrl = util.format(
+  mongoConfig.url,
+  mongoConfig.username,
+  mongoConfig.password
+)
 fastify.register(mongodb, {
-  url: ''
+  url: mongoUrl
 })
 
 // routes register
 fastify.register(goodsRoute)
 fastify.get('/', function (request, reply) {
-  reply.send({
-    hello: 'world'
-  })
+  reply.send('fastify restful api')
 })
 
 // 404 handler
@@ -30,7 +45,7 @@ fastify.setNotFoundHandler((request, reply) => {
 fastify.listen(4000)
   .then(() => {
     const { address, port } = fastify.server.address()
-    console.log(`server is running at ${address}${port}`)
+    console.log(`server is running at ${address}:${port}`)
   })
   .catch(err => {
     fastify.log.error(err)
