f515e825f03450cb31db0b22deb4b569de1eb125
jianglei <jweboy0630@gmail.com>
2018-03-10 16:48:06 +0800

    add class Mongodb and find function

--- route/goods/index.js
+++ route/goods/index.js
@@ -1,9 +1,14 @@
-module.exports = function (fastify, opts, next) {
-  fastify.get('/goods', function (request, reply) {
-    reply.send({
-      goods: true
-    })
-  })
+const Mongodb = require('../../lib/mongodb')
 
+module.exports = function (fastify, opts, next) {
+  fastify.get('/goods', async function (request, reply) {
+    const db = new Mongodb('goods', this.mongo)
+    try {
+      const data = await db.find()
+      reply.send(data)
+    } catch (err) {
+      throw err
+    }
+  })
   next()
 }
