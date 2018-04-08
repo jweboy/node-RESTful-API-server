b422e65aaf7535990f11fc39f8fdec7459a84d5e
jianglei <jweboy0630@gmail.com>
2018-04-08 22:28:07 +0800

    comment：mongodb connect for test upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const multipart = require('fastify-multipart')
 const leveldb = require('fastify-leveldb')
 const auth = require('fastify-auth')
 const routes = require('./route')
-const mongodb = require('./middleware/mongodb')
+// const mongodb = require('./middleware/mongodb')
 const authCfg = require('./config/auth')
 const authUtil = require('./util/auth')
 
@@ -21,9 +21,9 @@ fastify.decorate('verifyJWTandLevel', authUtil.verifyJWTandLevel)
 fastify.decorate('verifyUserAndPassword', authUtil.verifyUserAndPassword)
 
 // hooks
-fastify.addHook('onClose', function (fastify, done) {
-  fastify.mongodb.disconnect()
-})
+// fastify.addHook('onClose', function (fastify, done) {
+//   fastify.mongodb.disconnect()
+// })
 
 // form body register => parse x-www-form-urlencoded bodies
 fastify.register(formbody)
@@ -31,13 +31,13 @@ fastify.register(formbody)
 fastify.register(multipart)
 
 // mongodb register
-fastify.register(mongodb)
-  .after(err => {
-    if (err) {
-      throw err
-    }
-    console.log('db connect success!')
-  })
+// fastify.register(mongodb)
+//   .after(err => {
+//     if (err) {
+//       throw err
+//     }
+//     console.log('db connect success!')
+//   })
 
 // routes register
 fastify
