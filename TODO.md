c53cd291fec8ef4157f75250f9b1a1e24b737ca3
jianglei <jweboy0630@gmail.com>
2018-04-10 21:16:47 +0800

    update： listen port

--- TODO.md
+++ TODO.md
@@ -2,3 +2,4 @@
 
 - accessKey / secretKey 需要提取到 process.env变量
 - post /upload 增加接受的格式限制（目前只允许图片）
+- PORT需要提取到process里面
--- app.js
+++ app.js
@@ -64,7 +64,7 @@ fastify.setNotFoundHandler((request, reply) => {
 })
 
 // start server
-fastify.listen(4000)
+fastify.listen(3000)
   .then(() => {
     const { address, port } = fastify.server.address()
     console.log(`server is running at ${address}:${port}`)
