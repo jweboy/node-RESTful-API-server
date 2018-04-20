e74d0f004f5b5c4ea1f5088ff5ad28bb653c5729
jianglei <jweboy0630@gmail.com>
2018-04-20 23:31:55 +0800

    add: Access-Control-Allow-Methods with header

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ fastify.addHook('preHandler', function (req, reply, next) {
   // fastify.util(req, 'timestamp', new Date())
   // 设置cors,支持跨域
   reply.header('Access-Control-Allow-Origin', '*')
+  reply.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
   next()
 })
 // fastify.addHook('onClose', function (fastify, done) {
