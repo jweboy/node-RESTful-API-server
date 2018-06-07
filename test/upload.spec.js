7c232912926a6812115f57f8714bf71643a9132d
jianglei <1259524459@qq.com>
2018-06-07 21:46:14 +0800

    Update: temporarily hide the test code

--- test/upload.spec.js
+++ test/upload.spec.js
@@ -1,61 +1,61 @@
-process.env.NODE_ENV = 'test'
+// process.env.NODE_ENV = 'test'
 
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const fs = require('fs')
-const jsonSchema = require('chai-json-schema')
-const { getFileSuccess } = require('../schema/upload/getFile')
+// const chai = require('chai')
+// const chaiHttp = require('chai-http')
+// const fs = require('fs')
+// const jsonSchema = require('chai-json-schema')
+// const { getFileSuccess } = require('../schema/upload/getFile')
 
-const { server } = require('../app')
+// const { server } = require('../app')
 
-const expect = chai.expect
+// const expect = chai.expect
 
-chai.use(chaiHttp)
-chai.use(jsonSchema)
+// chai.use(chaiHttp)
+// chai.use(jsonSchema)
 
-// // // let fastify = null
-// // describe('Node RESTful API', () => {
-// //   describe('/api', () => {
-// //     // it('should delete picture with fileKey', (done) => { 
-// //     //   chai.request(app)
-// //     //     .delete('/api/upload/picture/:fileKey')
-// //     //     .end((err, res) => {
-// //     //       expect(err).to.be.null
-// //     //       expect(res).to.have.status(612)
-// //     //       expect(res).to.have.header('content-type', /application\/json/)
-// //     //       expect(res).to.be.json
-// //     //       done()
-// //     //     })
-// //     // })  
-// //   })
-// // })
+// // // // let fastify = null
+// // // describe('Node RESTful API', () => {
+// // //   describe('/api', () => {
+// // //     // it('should delete picture with fileKey', (done) => { 
+// // //     //   chai.request(app)
+// // //     //     .delete('/api/upload/picture/:fileKey')
+// // //     //     .end((err, res) => {
+// // //     //       expect(err).to.be.null
+// // //     //       expect(res).to.have.status(612)
+// // //     //       expect(res).to.have.header('content-type', /application\/json/)
+// // //     //       expect(res).to.be.json
+// // //     //       done()
+// // //     //     })
+// // //     // })  
+// // //   })
+// // // })
 
-describe('/api/upload', () => {
-  it('should PUT file to QiniuCloud', (done) => {
-      chai.request(server)
-        .put('/api/upload')
-        .attach('imageField', fs.readFileSync('static/nodejs.png'), 'nodejs.png')
-        .end((err, res) => { 
-          expect(err).to.be.null
-          expect(res).to.have.status(409)
-          expect(res).to.have.header('content-type', /application\/json/)
-          expect(res).to.be.json
+// describe('/api/upload', () => {
+//   it('should PUT file to QiniuCloud', (done) => {
+//       chai.request(server)
+//         .put('/api/upload')
+//         .attach('imageField', fs.readFileSync('static/nodejs.png'), 'nodejs.png')
+//         .end((err, res) => { 
+//           expect(err).to.be.null
+//           expect(res).to.have.status(409)
+//           expect(res).to.have.header('content-type', /application\/json/)
+//           expect(res).to.be.json
 
-          done()
-        })
-  })
+//           done()
+//         })
+//   })
   
-  it('should see picture list json', (done) => {
-      chai.request(server)
-        .get('/api/upload/list')
-        .query({ page: 1, size: 5 })
-        .end((err, res) => {
-          expect(err).to.be.null
-          expect(res).to.have.status(200)
-          expect(res).to.have.header('content-type', /application\/json/)
-          expect(res).to.be.json
-          expect(res).to.be.jsonSchema(getFileSuccess)
-          done()
-        })
-    })
-})
+//   it('should see picture list json', (done) => {
+//       chai.request(server)
+//         .get('/api/upload/list')
+//         .query({ page: 1, size: 5 })
+//         .end((err, res) => {
+//           expect(err).to.be.null
+//           expect(res).to.have.status(200)
+//           expect(res).to.have.header('content-type', /application\/json/)
+//           expect(res).to.be.json
+//           expect(res).to.be.jsonSchema(getFileSuccess)
+//           done()
+//         })
+//     })
+// })
