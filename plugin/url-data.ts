bfb075cdde103161455c6a568e13d95e6109d585
jweboy <1259524459@qq.com>
2018-07-11 14:12:13 +0800

    Add other file

---
+++ plugin/url-data.ts
@@ -0,0 +1,10 @@
+// import { UrlObject } from 'url'
+
+// // Extend FastifyReply with the "fastify-url-data" plugin
+// declare module 'fastify' {
+//   interface PluginUrl {
+//     urlData (): UrlObject
+//   }
+// }
+
+// export default PluginUrl
