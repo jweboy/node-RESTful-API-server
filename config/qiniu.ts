4c9b4585070d86ecb5caf9e56f0ff5c1e1577280
jweboy <1259524459@qq.com>
2018-08-10 16:39:36 +0800

    Add some *.d.ts for test

--- config/qiniu.ts
+++ config/qiniu.ts
@@ -1,7 +1,9 @@
-module.exports = {
+const config =  {
   accessKey: 'KgNS98Sj66CuXFi64xNHs11vfrO8iXmX8Zcht-Id',
   secretKey: 'gohLJusvDqZcwwYaL_DcF-KeTDX65zDdEzaEyayP',
   username: '15088683964@163.com',
   password: 'jl940630',
-  grantType: 'password'
+  grantType: 'password',
 }
+
+export default config
---
+++ typings/accepts.d.ts
@@ -0,0 +1,7 @@
+// import fastify = require('fastify')
+// // import accepts from 'fastify-accepts'
+
+// declare module "fastify-accepts" {
+//     const accepts: any;
+//     export default accepts
+// }
