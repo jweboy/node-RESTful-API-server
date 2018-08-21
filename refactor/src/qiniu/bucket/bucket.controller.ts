74e321ded2f1936878fbd3da1bb43c22b07aca84
jweboy <1259524459@qq.com>
2018-08-21 22:27:04 +0800

    Finish todo list

--- refactor/src/qiniu/bucket/bucket.controller.ts
+++ refactor/src/qiniu/bucket/bucket.controller.ts
@@ -1,21 +1,18 @@
 import { Controller, Get, Post, Body, Delete, Param, HttpCode, UseInterceptors, UsePipes } from '@nestjs/common';
 import { BucketService } from './bucket.service';
-import { AccessToken } from '../common/decorators/access-token.decorator';
+import { AccessToken, getAccessToken } from '../common/decorators/access-token.decorator';
 import { BucketInterceptor } from './bucket.interceptor';
 import { CreateBucketDto } from './dto/bucket.dto';
 import { ValidationPipe } from '../../common/pipes/validation.pipe';
+import { encode } from '../../common/util/base64-url';
 import * as config from './config.json';
-import { encode } from 'common/util/base64-url';
 import * as url from 'url';
-import * as qiniu from 'qiniu';
-import * as data from '../common/decorators/config.json';
 
 const { getUri, postUri } = (config as any);
 
-// TODO: AccessToken需要封装传参公有化
-
 // 控制器层负责处理传入的请求, 并返回对客户端的响应。
-@Controller('bucket')
+
+@Controller('qiniu/bucket')
 export class BucketController {
   constructor(private readonly bucketService: BucketService) {}
   @Post()
@@ -23,7 +20,7 @@ export class BucketController {
   create(@Body() createBucket: CreateBucketDto) {
     const region = createBucket.region || 'z0';
     const fullUri = url.resolve(postUri, `/mkbucketv2/${encode(createBucket.name)}/region/${region}`);
-    const token = qiniu.util.generateAccessToken(data as any, fullUri);
+    const token = getAccessToken(fullUri);
     return this.bucketService
       .create(fullUri, token)
       .toPromise()
@@ -46,7 +43,7 @@ export class BucketController {
   delete(@Param() params) {
     const name = params.name || '';
     const fullUri = url.resolve(postUri, `/drop/${name}`);
-    const token = qiniu.util.generateAccessToken(data as any, fullUri);
+    const token = getAccessToken(fullUri);
     return this.bucketService
       .delete(fullUri, token)
       .toPromise()
