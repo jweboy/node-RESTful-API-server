5c9461606e0ceeb75d73369cd5b58a6094f4d973
jweboy <1259524459@qq.com>
2018-09-16 14:47:13 +0800

    Upgrade pm2 conf #2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -45,7 +45,7 @@ module.exports = {
       ssh_options: 'StrictHostKeyChecking=no',
       'pre-setup':'echo "pm2 deploy"',
       'pre-deploy': 'git fetch && git pull origin master',
-      'post-deploy': 'npm install && pm2 startOrRestart',
+      'post-deploy': 'yarn && yarn restart',
       'env': {
         'NODE_ENV': 'production'
       }
